380f93460b709d34931a03b6d2188040a82dc01f
mahao <17888102352@163.com>
2022-08-29 21:33:54 +0800

    .

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,58 +2,68 @@ import fabricjs from "../lib/fabricjs/sidebar.js";
 import vueuse from "../lib/vueuse/sidebar.js";
 
 export default {
-    title: 'iBook',
-    description: 'My Personal Library.',
-    ignoreDeadLinks: true,
+  title: "iBook",
+  description: "My Personal Library.",
+  ignoreDeadLinks: true,
 
-    // Theme related configurations.
-    themeConfig: {
-        docFooter: {
-            prev: '上一节',
-            next: '下一节'
-        },
-        nav: [
-            { text: '基础', link: '/base/html/' },
-            {
-                text: '第三方库', items: [
-                    { text: 'Fabric.js', link: '/lib/fabricjs/intro' },
-                    { text: 'VueUse', link: '/lib/vueuse/state/createGlobalState' },
-                ]
-            },
-            { text: '面试题', link: '/interview/js/on' },
-            { text: '常见问题记录', link: '/solution/' },
-            { text: '接下来...', link: '/todo' },
+  // Theme related configurations.
+  themeConfig: {
+    docFooter: {
+      prev: "上一节",
+      next: "下一节",
+    },
+    nav: [
+      { text: "开始", link: "/startup/devEnv" },
+      { text: "前端基础", link: "/base/html/index" },
+      {
+        text: "第三方库",
+        items: [
+          { text: "Fabric.js", link: "/lib/fabricjs/intro" },
+          { text: "VueUse", link: "/lib/vueuse/state/createGlobalState" },
         ],
-        sidebar: {
-            '/lib/fabricjs': fabricjs,
-            '/lib/vueuse/': vueuse,
-            '/base/': [
-                {
-                    text: 'HTML',
-                    collapsible: true,
-                    items: [
-                        { text: '第一节', link: '/base/html/' },
-                    ]
-                },
-                {
-                    text: 'CSS',
-                    collapsible: true,
-                    items: [
-                        { text: 'display', link: '/base/css/display' },
-                        { text: '冷门属性', link: '/base/css/uncommon' },
-                    ]
-                },
-            ],
-            '/interview/': [
-                {
-                    text: 'JavaScript',
-                    collapsible: true,
-                    items: [
-                        { text: '面试', link: '/interview/js/on' },
-                        { text: '笔试', link: '/interview/js/off' },
-                    ]
-                }
-            ]
-        }
-    }
-}
+      },
+      { text: "面试题", link: "/interview/js/on" },
+      { text: "常见问题记录", link: "/solution/" },
+      { text: "接下来...", link: "/todo" },
+    ],
+    sidebar: {
+      "/startup/": [
+        {
+          text: "vscode",
+          collapsible: true,
+          items: [
+            { text: "插件 plugins", link: "/startup/vscode/plugin" },
+            { text: "设置 settings", link: "/startup/vscode/setting" },
+          ],
+        },
+      ],
+      "/lib/fabricjs": fabricjs,
+      "/lib/vueuse/": vueuse,
+      "/base/": [
+        {
+          text: "HTML",
+          collapsible: true,
+          items: [{ text: "第一节", link: "/base/html/index" }],
+        },
+        {
+          text: "CSS",
+          collapsible: true,
+          items: [
+            { text: "display", link: "/base/css/display" },
+            { text: "冷门属性", link: "/base/css/uncommon" },
+          ],
+        },
+      ],
+      "/interview/": [
+        {
+          text: "JavaScript",
+          collapsible: true,
+          items: [
+            { text: "面试", link: "/interview/js/on" },
+            { text: "笔试", link: "/interview/js/off" },
+          ],
+        },
+      ],
+    },
+  },
+};
---
+++ docs/startup/devEnv.md
@@ -0,0 +1,15 @@
+# 搭建你的开发环境
+
+## 安装 vscode
+
+https://code.visualstudio.com/
+
+参考：
+    [vscode-plugin]
+
+## 安装 git
+
+https://git-scm.com/
+
+
+## 安装
